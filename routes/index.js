c0a2e1e690658ab0aebbb47070256a38bcff448d
scaev <kenkaraa@gmail.com>
2023-01-22 12:29:08 -0500

    Adds Login/Logout Routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    // Requesting the user's profile and email
+    scope: ["profile", "email"],
+  })
+);
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/movies',
+    failureRedirect: '/movies'
+  }
+));
+
+// OAuth logout route
+router.get("/logout", function (req, res) {
+  req.logout(function () {
+    res.redirect("/movies");
+  });
 });
 
 module.exports = router;
