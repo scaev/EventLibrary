01b91ca352a181bec94e932db451e620d2137843
scaev <kenkaraa@gmail.com>
2023-01-22 12:22:51 -0500

    Modify googleStrategy

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,40 @@
 const passport = require("passport");
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const User = require("../models/user");
 
 passport.use(
-    new GoogleStrategy(
-      // Configuration object
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK,
-        },
-        async function (accessToken, refreshToken, profile, cb) {
-        }
-        ));
+  new GoogleStrategy(
+    // Configuration object
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK,
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+      // A user has logged in with OAuth...
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (user) return cb(null, user);
+        user = await User.create({
+          name: profile.displayName,
+          googleId: profile.id,
+          email: profile.emails[0].value,
+          avatar: profile.photos[0].value,
+        });
+        return cb(null, user);
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  )
+);
+
+//to check is the password still verified (or valid)
+passport.serializeUser(function (user, cb) {
+  cb(null, user._id);
+});
+
+passport.deserializeUser(async function (userId, cb) {
+  const user = await User.findById(userId);
+  cb(null, user);
+});
