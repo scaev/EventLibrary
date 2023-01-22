d83abb9eaf84c2c83211089129595ea54b659b66
scaev <kenkaraa@gmail.com>
2023-01-22 12:12:59 -0500

    Modify user model

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,15 @@
 const passport = require("passport");
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require("../models/user");
+
+passport.use(
+    new GoogleStrategy(
+      // Configuration object
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK,
+        },
+        async function (accessToken, refreshToken, profile, cb) {
+        }
+        ));
