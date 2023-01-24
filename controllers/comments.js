a3ea563cbe9c43a9f988afb6bb5ca77eba3f64e7
scaev <kenkaraa@gmail.com>
2023-01-24 11:49:27 -0500

    Adds route and controller for deleting comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,7 +5,7 @@ const Event = require("../models/event");
 function create(req, res) {
     Event.findById(req.params.id, function (err, event) {
       // We push an object with the data for the
-      // review subdoc into Mongoose arrays
+      // comment subdoc into Mongoose arrays
       req.body.user = req.user._id;
       req.body.userName = req.user.name;
       req.body.userAvatar = req.user.avatar;
@@ -16,7 +16,22 @@ function create(req, res) {
     });
   }
 
+  async function deleteComment(req, res, next) {
+    try {
+      const event = await Event.findOne({
+        "comments._id": req.params.id,
+        "comments.user": req.user._id,
+      });
+      if (!event) return res.redirect("/events");
+      event.comments.remove(req.params.id);
+      await event.save();
+      res.redirect(`/events/${event._id}`);
+    } catch (err) {
+      return next(err);
+    }
+  }
 
 module.exports = {
-    create,
+  create,
+  delete: deleteComment,
   };
--- routes/comments.js
+++ routes/comments.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const commentsCtrl = require("../controllers/comments");
 
 router.post("/events/:id/comments", commentsCtrl.create);
+router.delete("/comments/:id", commentsCtrl.delete);
 
 module.exports = router;
