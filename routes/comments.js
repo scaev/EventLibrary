3e145009f74888e1be9da99a940bc02e0e9d7d0d
scaev <kenkaraa@gmail.com>
2023-01-24 12:10:21 -0500

    Adds ensureLoggedIn function for security

--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 const commentsCtrl = require("../controllers/comments");
 
-router.post("/events/:id/comments", commentsCtrl.create);
-router.delete("/comments/:id", commentsCtrl.delete);
+function ensureLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/");
+  }
+
+router.post("/events/:id/comments", ensureLoggedIn, commentsCtrl.create);
+router.delete("/comments/:id", ensureLoggedIn, commentsCtrl.delete);
 
 module.exports = router;
--- routes/events.js
+++ routes/events.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const eventsCtrl = require("../controllers/events");
 
-router.get("/", eventsCtrl.index);
-router.get("/new", eventsCtrl.new);
-router.post("/", eventsCtrl.create);
-router.get("/:id", eventsCtrl.show);
+function ensureLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/");
+}
+
+router.get("/", eventsCtrl.index); //(display all events)
+router.get("/new", ensureLoggedIn, eventsCtrl.new); //(display a form for entering a new event)
+router.post("/", eventsCtrl.create); //(display a "detail/show" page for a single event)
+router.get("/:id", ensureLoggedIn, eventsCtrl.show); //(handle the new form being submitted)
 
 module.exports = router;
